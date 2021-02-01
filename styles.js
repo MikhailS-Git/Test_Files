5c133bc78fbb262af478aadb800763c7e5381abf
MikhailS-Git <78349983+MikhailS-Git@users.noreply.github.com>
2021-02-01 15:26:22 +0300

    Update and rename styles.css to styles.js

---
+++ styles.js
@@ -0,0 +1,18 @@
+var css= ".container {width: 200px;height: 75px;position: fixed;top: 20px;left: 50%;transform: translateX(-50%);background: red;}"
+
+var head = document.head || document.getElementsByTagName('head')[0],
+
+var style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+
+if (style.styleSheet)
+{
+	style.styleSheet.cssText = css;
+}
+else
+{
+	style.appendChild(document.createTextNode(css));
+}
